bf6b4d33d66c6ae3e1607e7ebe74150d315c31a5
JohnWhity <ilya-kondratev-2002@mail.ru>
2022-10-25 21:13:42 +0300

    Вариант 7.	Дан файл целых чисел, получить точные квадраты. Вариант 7.	Дописать метод для нахождения количества точных квадратов.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 file = open("Numbers", 'r')
+c = 0
 try:
     while True:
         x = int(file.readline())
         y = x**2
         print(y)
+        c += 1
         if x == "":
             break
 except(ValueError):
-    print()
+    print(c)
+
